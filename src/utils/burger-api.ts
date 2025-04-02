deaad4029b41f7b12d842545b68756dba8073e53
Dmitry Filev <dmitryf41@gmail.com>
2025-04-02 12:30:15 +0300

    Project 12 version 1.0

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -114,7 +114,9 @@ export const orderBurgerApi = (data: string[]) =>
     body: JSON.stringify({
       ingredients: data
     })
-  }).then((data) => {console.log('!!!!!!!!!');console.log(data)
+  }).then((data) => {
+    console.log('!!!!!!!!!');
+    console.log(data);
     if (data?.success) return data;
     return Promise.reject(data);
   });
